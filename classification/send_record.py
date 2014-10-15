d1e8cb017d8a762d2bb06aae39ca16fac7ac6b34
Sile Hu <husile@ufl.edu>
2014-10-14 20:48:44 -0400

    init commit on classification; socketTextStream

---
+++ classification/send_record.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import socket
+from time import sleep
+from random import randint
+
+text_file = open("sample_libsvm_data.txt", "r")
+lines = text_file.readlines()
+filelen = len(lines)
+
+TCP_IP = '127.0.0.1' 
+TCP_PORT = 9999 
+BUFFER_SIZE = 1024
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(20)
+
+conn, addr = s.accept()
+print 'Connection address:', addr
+
+while 1:
+  line = lines[randint(0, filelen-1)]
+  print line
+  conn.send(line)
+  sleep(1)
+conn.close()
+

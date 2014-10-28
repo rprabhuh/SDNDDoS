c040e62347ff091c921e443a84e726aea9fcb44a
Rahul H. Prabhu <rprabhu@lin309-09.cise.ufl.edu>
2014-10-27 20:29:31 -0400

    Modified DecisionTree algorithm to work with Categorical data.

--- classification/DecisionTree.py
+++ classification/DecisionTree.py
@@ -23,20 +23,22 @@ def parsePoint(line):
     values = [float(x) for x in line.split(',')]
     return LabeledPoint(values[0], values[1:])
 
+
 # Read the input data file
 data = sc.textFile("dataset.dat")
 
 # Parse the data to construct a classification dataset
 parsedData = data.map(parsePoint)
 
-
+#Indicate the categorical attributes
+#categorical_attributes = {2:2,3:3,4:3}
 # Build the classification model
 '''
 impurity can be any of {gini, entropy, variance}
 categoricalFeaturesInfo contains information pertaining to categorical features in the dataset
 '''
-model = DecisionTree.trainClassifier(parsedData, numClasses=2, categoricalFeaturesInfo={},
-                                     impurity='gini', maxDepth=5, maxBins=100)
+model = DecisionTree.trainClassifier(parsedData, numClasses=2, categoricalFeaturesInfo={1:3,2:11,3:5},
+                                     impurity='gini', maxDepth=10, maxBins=50000)
 # Evaluating the model on training data
 
 predictions = model.predict(parsedData.map(lambda x: x.features))
